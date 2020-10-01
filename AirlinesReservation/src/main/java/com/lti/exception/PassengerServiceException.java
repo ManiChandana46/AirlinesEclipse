8e7967bd63b28fe902d6896444ece0d18997ee35
anshul <anshsharma4650@gmail.com>
2020-10-01 14:31:30 +0530

    Added Code for AddPassenger Service class

---
+++ AirlinesReservation/src/main/java/com/lti/exception/PassengerServiceException.java
@@ -0,0 +1,31 @@
+package com.lti.exception;
+
+public class PassengerServiceException extends RuntimeException {
+
+	public PassengerServiceException() {
+		super();
+
+	}
+
+	public PassengerServiceException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+
+	}
+
+	public PassengerServiceException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public PassengerServiceException(String message) {
+		super(message);
+
+	}
+
+	public PassengerServiceException(Throwable cause) {
+		super(cause);
+
+	}
+
+}
---
+++ AirlinesReservation/src/main/java/com/lti/service/PassengerService.java
@@ -0,0 +1,11 @@
+package com.lti.service;
+
+import java.util.List;
+
+import com.lti.entity.PassengerDetail;
+
+public interface PassengerService {
+
+	void addPassengers(List<PassengerDetail> passengers);
+
+}
