5c8ca0580e1335764e3474b7df5a7ba90ea5cd83
Sayan-Mitra-007 <sayanmitra098@gmail.com>
2020-10-02 17:02:13 +0530

    Warnings removed!!

--- AirlinesReservation/src/main/java/com/lti/repository/BookingDeatailRepo.java
+++ AirlinesReservation/src/main/java/com/lti/repository/BookingDeatailRepo.java
@@ -1,7 +1,6 @@
 package com.lti.repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
--- AirlinesReservation/src/main/java/com/lti/repository/RegisterRepo.java
+++ AirlinesReservation/src/main/java/com/lti/repository/RegisterRepo.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.lti.dto.RegisterDto;
 import com.lti.entity.CustomerDetail;
 
 @Repository
